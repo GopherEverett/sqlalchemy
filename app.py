08a31718f98ec5126e2b9f557d53b9fae007d98c
Andre Pato <36831606+anpato@users.noreply.github.com>
2021-04-13 14:33:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 from resources.user import Users, SingleUser
 
 app = Flask(__name__)
-
+CORS(app)
 api = Api(app)
 
 # Init db and migrate here
